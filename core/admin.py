22daa14727f7d200de53f45abd07c0c678b02ace
LincolnBinda <lincoln.binda.silva@gmail.com>
2021-02-23 21:57:10 -0300

    De hoje(novamente)

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import MarcaVeiculo, Veiculo, Pessoa, Parametros
+from .models import MarcaVeiculo, Veiculo, Pessoa, Parametros, MovRotativo
+
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
 
 
 admin.site.register(MarcaVeiculo)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
+admin.site.register(MovRotativo, MovRotativoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+import math
 
 
 class Pessoa(models.Model):
@@ -33,3 +35,24 @@ class Parametros(models.Model):
 
     def __str__(self):
         return 'Parametros Gerais'
+
+class MovRotativo(models.Model):
+    checkin = models.DateTimeField(auto_now=False)
+    checkout = models.DateTimeField(auto_now=False, blank=True, null=True)
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2)
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    pago = models.BooleanField(default=False)
+
+    def __str__(self):
+        # pylint: disable=E1101
+        return self.veiculo.placa
+
+    def total(self):
+        return self.valor_hora * self.horas_total()
+
+    def horas_total(self):
+        if self.checkout is None:
+            return math.ceil((self.checkin - self.checkin).total_seconds() / 3600)
+        else:
+            return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
+
