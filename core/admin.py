35c8d63afcb02190488b1a42c27158c0ac8c2b9a
LincolnBinda <lincoln.binda.silva@gmail.com>
2021-03-11 11:00:38 -0300

    Esquentando...

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MarcaVeiculo, Veiculo, Pessoa, Parametros, MovRotativo
+from .models import MarcaVeiculo, Veiculo, Pessoa, Parametros, MovRotativo, Mensalista
 
 
 class MovRotativoAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
 admin.site.register(MovRotativo, MovRotativoAdmin)
+admin.site.register(Mensalista)
--- core/models.py
+++ core/models.py
@@ -56,3 +56,15 @@ class MovRotativo(models.Model):
         else:
             return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
 
+class Mensalista(models.Model):
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    inicio = models.DateField()
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.veiculo) + ' - ' + str(self.valor_mes)
+
+class MovMensalista(models.Model):
+    mensalista = models.ForeignKey(Mensalista)
+    dt_pgto = models.DateField()
+    total = models.DecimalField(max_digits=6, decimal_places=2)
